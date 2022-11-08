5f57803c1d41279f47c47a6d90694f7156a628c6
zAle711 <alessandro7111@gmail.com>
2022-11-08 22:51:05 +0100

    Added kdeplot (pdf) and boxplot

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("PROGETTO MACHINE LEARNING")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.expand_frame_repr', True)
+
+df = pd.read_csv('dataset/water_potability.csv')
+
+print( "Colonne: ", list(df.columns), "\nSize: ", df.size)
+def pot(col):
+    c = col[8]
+    return 'Yes' if c == 1 else 'No'
+
+print(df['Potability'].replace(1, 'Yes'))
+print(df['Potability'].replace(0, 'No'))
+
+
+sns.boxplot(df, x='ph', y='Potability')
+plt.tight_layout()
+plt.show()
